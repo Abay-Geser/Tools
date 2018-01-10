3b191fcc80abdf33b38068c3c401fbec4fa547e3
Abay-Geser <abay.geser@gmail.com>
2018-01-10 19:39:13 +0300

    psexec deploy & run jar

---
+++ psexec/deploy/remote script.bat
@@ -0,0 +1,3 @@
+cd  C:\path
+taskkill /im java.exe
+java -jar file.jar >> startup.log
---
+++ psexec/deploy/start.bat
@@ -0,0 +1,2 @@
+cd C:\TeamCity\psexec\PSTools
+PsExec.exe \\IP -u "PC\User" -p "pass" -s -d cmd.exe /c "C:\path\run.bat"
